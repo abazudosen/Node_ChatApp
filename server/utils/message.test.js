4bcd94ba2308b46e50a28bbf149194d92226f319
abazudosen <abudosen@gmail.com>
2017-10-24 11:07:20 +0100

    add geolocation support via geolocaton API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', ()=> {
     it ('should generate correct message object', ()=>{
@@ -12,3 +12,16 @@ describe('generateMessage', ()=> {
         expect(message).toInclude({from, text});
     });
 }); 
+
+describe('generateLocationMessage', ()=> {
+    it('should generate correct location object', ()=> {
+       var from = 'abaz';
+       var lat = 13;
+       var lng = 19;
+       var url = 'https://www.google.com/maps?q=13,19';
+       var message = generateLocationMessage(from, lat, lng);
+
+       expect(message.createdAt).toBeA('number');
+       expect(message).toInclude({from, url});
+    });
+});
