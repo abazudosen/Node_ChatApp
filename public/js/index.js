090ac433e2ae04e5a6cfa1dc4f6ceca35ec3ab35
abazudosen <abudosen@gmail.com>
2017-10-27 13:00:24 +0100

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,23 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
-
-    // socket.emit('createEmail', {
-    //     to: 'jen@test.com',
-    //     text: ' this is you!'
-    // });
 
 });
 
@@ -14,9 +25,6 @@ socket.on('disconnect', function() {
     console.log('disconnected from server');
 });
 
-// socket.on('newEmail', function(email) {
-//     console.log('new email', email);
-// });
 
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -28,7 +36,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 // socket.emit('createMessage', {
@@ -46,7 +54,9 @@ socket.on('newLocationMessage', function (message) {
         url: message.url,
         createdAt: formattedTime
     });
+
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 
